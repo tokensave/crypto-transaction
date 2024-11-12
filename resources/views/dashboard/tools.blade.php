686c665d54be46cf845f5bec3876ce15e8237722
tokensave <tokensave@yandex.ru>
2024-11-13 00:10:31 +0300

    add new dashboard

--- resources/views/dashboard/tools.blade.php
+++ resources/views/dashboard/tools.blade.php
@@ -93,18 +93,4 @@
         @endisset
     </div>
 
-{{--    <div class="flex justify-center my-8">--}}
-{{--        <div class="text-white text-center bg-gray-900 p-4 rounded-lg">--}}
-{{--            <h2 class="text-lg font-semibold mb-4">Текущие курсы криптовалют</h2>--}}
-{{--            <ul>--}}
-{{--                @foreach($cryptoRates as $pair => $rate)--}}
-{{--                    <li class="mb-2">--}}
-{{--                        <span class="font-semibold">{{ $pair }}:</span>--}}
-{{--                        <span>{{ $rate ? number_format($rate, 2) : 'Не доступно' }} USD</span>--}}
-{{--                    </li>--}}
-{{--                @endforeach--}}
-{{--            </ul>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-
 </x-layouts.base>
